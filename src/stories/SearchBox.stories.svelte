40828fec0d79b992c5e4f026e9871d5cfb136ffa
Robin Roschlau <robin.roschlau@gmail.com>
2021-06-14 19:19:00 +0200

    Add storybook

---
+++ src/stories/SearchBox.stories.svelte
@@ -0,0 +1,20 @@
+
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import SearchBox from '../components/SearchBox.svelte'
+</script>
+
+<Meta
+    title="SearchBox"
+    component={SearchBox}
+    argTypes={{}}
+/>
+
+<Template let:args>
+    <SearchBox {...args} />
+</Template>
+
+<Story
+    name="Default"
+    args={{}}
+/>
